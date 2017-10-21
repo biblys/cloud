9a7accbc45493fdfa87f975b1c34f8dc1abeff64
Clement Bourgoin <cb@nokto.net>
2017-10-21 11:34:49 +0200

    Add Stripe secret key to config file

--- config.dist.js
+++ config.dist.js
@@ -1,7 +1,8 @@
 const config = {
   MONGO_URL: 'mongodb://localhost/biblys-cloud',
   PORT: '3000',
-  STRIPE_PUBLIC_KEY: 'pk_test_xxxxxxxxxxxxxxxxxxxxxxxxxx'
+  STRIPE_PUBLIC_KEY: 'pk_test_xxxxxxxxxxxxxxxxxxxxxxxx',
+  STRIPE_SECRET_KEY: 'sk_test_xxxxxxxxxxxxxxxxxxxxxxxx'
 };
 
 module.exports = config;
