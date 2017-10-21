2629e8987a645f8f00819865d2815471eaea0471
Clement Bourgoin <cb@nokto.net>
2017-10-21 11:35:05 +0200

    Process Stripe payment

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const router = express.Router();
-const config = require('../config.js');
+const router  = express.Router();
+const config  = require('../config.js');
 
 const Invoice = require('../models/invoice');
 
@@ -14,13 +14,42 @@ router.get('/:id', function(request, response, next) {
     }
 
     response.render('invoice', {
-      invoice: {
-        number: invoice.number,
-        name: invoice.name,
-        email: invoice.email,
-        amount: invoice.amount,
-      },
+      invoice: invoice,
       stripe_public_key: config.STRIPE_PUBLIC_KEY
+    });
+
+  });
+
+});
+
+router.post('/:id/process-payment', function(request, response, next) {
+
+  Invoice.findById(request.params.id, function(err, invoice) {
+
+    if (invoice === null) {
+      next(err);
+      return;
+    }
+
+    // Get stripe key from config and token from request
+    const stripe = require('stripe')(config.STRIPE_SECRET_KEY),
+      token = request.body.stripeToken;
+
+    stripe.charges.create({
+      amount: invoice.amount,
+      currency: 'eur',
+      description: `Facture n° ${invoice.number}`,
+      source: token,
+    }, function(err) {
+
+      if (err) {
+        next(err);
+        return;
+      }
+
+      // Todo: update invoice.payed & invoice.payedAt
+
+      response.redirect(`/invoices/${invoice._id}`);
     });
 
   });
