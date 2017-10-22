50e8cc71fdfea14808585f4eb7e8c5262406c966
Clement Bourgoin <cb@nokto.net>
2017-10-22 18:01:04 +0200

    Improve design

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Security headers
 app.use(function(request, response, next) {
-  response.setHeader('Content-Security-Policy', "default-src 'none'; connect-src https://checkout.stripe.com; script-src https://checkout.stripe.com https://axys.me; style-src 'self' https://checkout.stripe.com https://axys.me; img-src https://q.stripe.com https://axys.me; frame-src https://checkout.stripe.com; frame-ancestors 'none'; base-uri 'none'; object-src 'none'");
+  response.setHeader('Content-Security-Policy', "default-src 'none'; connect-src https://checkout.stripe.com; script-src https://checkout.stripe.com https://axys.me; style-src 'self' https://checkout.stripe.com https://axys.me; img-src 'self' https://q.stripe.com https://axys.me; frame-src https://checkout.stripe.com; frame-ancestors 'none'; base-uri 'none'; object-src 'none'");
   response.setHeader('X-Frame-Options', 'DENY');
   response.setHeader('X-Content-Type-Options', 'nosniff');
   response.setHeader('X-XSS-Protection', '1; mode=block');
