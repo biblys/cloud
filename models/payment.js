b8088e20ef629cab5dfdffc749d0015b7e7a23fe
Clement Bourgoin <cb@nokto.net>
2017-10-22 18:30:57 +0200

    Add a Payment model

---
+++ models/payment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const PaymentSchema = new mongoose.Schema({
+  axysId: {
+    type: String,
+    required: true
+  },
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: true
+  },
+  invoice: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Invoice',
+    required: true
+  },
+  amount: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Payment', PaymentSchema);
