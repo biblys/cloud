13d4679c9612009044587ac0cfc75847fe65dc97
Clement Bourgoin <cb@nokto.net>
2017-10-23 14:38:13 +0200

    Add an admin dashboard page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(request, response, next) {
+const auth      = require('../middlewares/auth');
+const authAdmin = require('../middlewares/authAdmin');
+
+// Home page
+router.get('/', function(request, response) {
   response.render('index');
+});
+
+// Admin dashboard
+router.get('/admin/', auth, authAdmin, function(request, response) {
+  response.render('admin');
 });
 
 module.exports = router;
