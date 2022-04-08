eb22add0cde1f4ea2b69337f0d23dacc98e4d569
Clément Bourgoin <c@iwzr.fr>
2022-04-08 23:20:18 +0200

    feat: require key header for authentification

--- netlify/functions/stripe-portal.js
+++ netlify/functions/stripe-portal.js
@@ -1,12 +1,19 @@
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 exports.handler = async function (event, context) {
+  const headers = event.headers;
+  if (!headers.key || headers.key !== process.env.BIBLYS_CLOUD_KEY) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Unauthorized' }),
+    };
+  }
+
   const params = event.queryStringParameters;
   const customerId = params.customer_id;
-
   const session = await stripe.billingPortal.sessions.create({
     customer: customerId,
-    return_url: 'https://example.com/account',
+    return_url: 'https://biblys.fr/',
   });
 
   return {
